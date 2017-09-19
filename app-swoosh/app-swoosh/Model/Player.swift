25de9dd2d7ada14c02129545f9638358075b9236
Steffen Jordan Meilsoe <Steffen@vlan-2723-10-17-251-101.guest.wireless.sydney.edu.au>
2017-09-20 09:30:34 +1000

    added additional code and fixes

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Steffen Jordan Meilsoe on 9/19/17.
+//  Copyright © 2017 Lucrement IVS. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
